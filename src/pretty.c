248d7710b91820d7a0efab4982e8af6d54355c74
Peter Beerli <pbeerli@fsu.edu>
2025-12-26 22:31:35 -0500

    buffer overunr fixed

--- src/pretty.c
+++ src/pretty.c
@@ -2478,7 +2478,7 @@ also very common that some migration routes are estimated poorly \
 because the data contains little or no information for that route. \
 Increasing the prior range will not help in such situations, but \
 reducing the number of parameters may help.";
-    section = (char *) mycalloc(LINESIZE,sizeof(char));
+    section = (char *) mycalloc(LONGLINESIZE,sizeof(char));
     mysnprintf(title,LINESIZE,"Potential Problems");
     pdf_new_page("");
     HPDF_Page_SetFontAndSize (page, helvob, 16.0);
@@ -2521,8 +2521,10 @@ reducing the number of parameters may help.";
     pdf_advance(&page_height);
     if(world->warningsize > 0)
     {
-        buffer = (char *) mycalloc(strlen(world->warning)+1,sizeof(char));
-        mysnprintf(buffer,LINESIZE,"%s",world->warning);
+      	size_t buflen=strlen(world->warning)+1;
+        buffer = (char *) mycalloc(buflen,sizeof(char));
+
+        mysnprintf(buffer,buflen,"%s",world->warning);
         b = buffer;
         tmp = strsep(&buffer,"\n");
         while(tmp!=NULL)
